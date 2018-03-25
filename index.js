2c9aaad3a8bc6386d95175747aeabec139fd5c00
Michał Stocki <michal.stocki@uxpin.com>
2018-03-25 01:32:58 +0100

    Reading commands from cmd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Hello world');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  switch (input) {
+    case 'exit':
+      rl.close();
+      break;
+    default:
+      console.log(`Received: ${input}`);
+  }
+});
