00d1ece7058f89f1351d87a8c18d28cba75546a2
Michał Stocki <michal.stocki@uxpin.com>
2018-03-25 03:17:43 +0200

    Switch to next station with "next" command

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const readline = require('readline');
 const { exec } = require('child_process');
 const {join} = require('path');
+const http = require('http');
+const authHeader = require('basic-auth-header');
+
 const HTTP_PASSWORD = 'aaa';
 
 const rl = readline.createInterface({
@@ -26,7 +29,37 @@ rl.on('line', (input) => {
       vlcProcess.kill();
       process.exit(0);
       break;
+    case 'next':
+      handleNext();
+      break;
     default:
-      console.log(`Received: ${input}`);
+      console.log(`Unknown command: "${input}".
+Available commands:
+  exit – Closes the program
+  next – Switch to the next program`);
   }
 });
+
+function handleNext() {
+  console.log('Playing next station');
+  sendCommand('pl_next');
+}
+
+function sendCommand(commandName) {
+  const requestOptions = {
+    hostname: 'localhost',
+    port: 8080,
+    path: `/requests/status.xml?command=${commandName}`,
+    method: 'GET',
+    headers: {
+      Authorization: authHeader('', HTTP_PASSWORD)
+    }
+  };
+  const req = http.request(requestOptions, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+  });
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  req.end();
+}
